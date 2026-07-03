75e103c184d836a8be41eeb97bca59fb91ee5a65
charan210 <83117623+charan210@users.noreply.github.com>
2026-07-03 19:39:23 +0530

    Time: 7 ms (38.83%) | Memory: 24 MB (31.01%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,17 +5,13 @@ public:
         sort(in.begin(),in.end());
         vector<vector<int>> ans;
         for(int i=0;i<n;i++){
-            int st=in[i][0];
-            int end=in[i][1];
-            if(!ans.empty()&&end<=ans.back()[1]) continue;
+           if(ans.empty() || in[i][0]>ans.back()[1]){
+              ans.push_back(in[i]);
+           }
 
-            for(int j=i+1;j<n;j++){
-                if(end>=in[j][0]){
-                    end=max(end,in[j][1]);
-                }
-                else break;
-            }
-            ans.push_back({st,end});
+           else{
+            ans.back()[1]=max(ans.back()[1],in[i][1]);
+           }
         }
         return ans;
     }

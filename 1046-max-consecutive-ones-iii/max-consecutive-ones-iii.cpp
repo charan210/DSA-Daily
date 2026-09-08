0cfda3a7e9c9d4659f312ab5d93cbf8604257231
charan210 <83117623+charan210@users.noreply.github.com>
2026-09-08 11:35:01 +0530

    Time: 6 ms (9.46%) | Memory: 69.5 MB (70.27%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -4,7 +4,7 @@ public:
         int l=0,r=0,ml=0,zeroes=0;
         while(r<nums.size()){
             if(nums[r]==0) zeroes++;
-        while(zeroes>k){
+        if(zeroes>k){
             if(nums[l]==0){
                 zeroes--;
                 l++;

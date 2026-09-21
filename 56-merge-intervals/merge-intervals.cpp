2a4395eb3401c2b7abd6cad41515036038c4e8e5
charan210 <83117623+charan210@users.noreply.github.com>
2026-09-21 13:17:06 +0530

    Time: 3 ms (89.34%) | Memory: 23.9 MB (60.36%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& in) {
         int n=in.size();
-        sort(in.begin(),in.end());
-        vector<vector<int>> ans;
-        for(int i=0;i<n;i++){
-           if(ans.empty() || in[i][0]>ans.back()[1]){
-              ans.push_back(in[i]);
-           }
-
-           else{
-            ans.back()[1]=max(ans.back()[1],in[i][1]);
-           }
+       sort(in.begin(),in.end());
+       vector<vector<int>> ans;
+       for(int i=0;i<n;i++){
+        if(ans.empty()|| in[i][0]>ans.back()[1]){
+            ans.push_back(in[i]);
         }
-        return ans;
+        else{
+            ans.back()[1]=max(ans.back()[1],in[i][1]);
+        }
+       }
+       return ans;
     }
 };
